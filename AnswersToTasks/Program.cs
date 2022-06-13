6763ce92dbeae6ceef4f2cf38bc0bbd3b91a2aec
Olya <Zlobinaolya87@gmail.com>
2022-06-13 19:24:18 +0300

    Third task done

--- AnswersToTasks/Program.cs
+++ AnswersToTasks/Program.cs
@@ -1,4 +1,5 @@
-﻿// Программа, которая на вход принимает 2 числа и выдает, какое число больше, а какое меньше.
+﻿// 1. Программа, которая на вход принимает 2 числа и выдает, какое число больше, а какое меньше.
+
 //Console.WriteLine("Input first number: ");
 //int A = Convert.ToInt32(Console.ReadLine());
 //Console.WriteLine("Input second number: ");
@@ -10,23 +11,36 @@
 //else
     //Console.WriteLine("Second number is more, first number is less  ");
 
-// Программа, которая принимает 3 числа и выдает максимальное из них.
-Console.Write("Input first number: ");  
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input third number: ");
-int num3 = Convert.ToInt32(Console.ReadLine());
-int max = num1;
-    if(num1 > num2){
-    Console.WriteLine("First number is maximum ");
-} 
+// 2. Программа, которая принимает 3 числа и выдает максимальное из них.
+
+//Console.Write("Input first number: ");  
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Input second number: ");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Input third number: ");
+//int num3 = Convert.ToInt32(Console.ReadLine());
+//int max = num1;
+    //if(num1 > num2){
+    //Console.WriteLine("First number is maximum ");
+//} 
+//else
+    //if (num2 > num3){
+    //Console.WriteLine("Second number is maximum ");
+//}
+//else    
+    //if (num3 > num1){
+    //Console.WriteLine("Third number is maximum");
+//}
+
+// 3. Написать программу, которая принимает число и проверяет четное ли оно (делится ли оно на 2 без остатка).
+
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num%2==0){
+    Console.WriteLine("Even Number");
+}
 else
-    if (num2 > num3){
-    Console.WriteLine("Second number is maximum ");
-}
-else    
-    if (num3 > num1){
-    Console.WriteLine("Third number is maximum");
-}
+Console.WriteLine("Odd Number");
+
+    
 
